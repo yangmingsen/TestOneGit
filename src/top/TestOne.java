2813d247c0312af4532a55d4301cd0013518bf85
yangmingsen <yangmingsen16@outlook.com>
2019-05-19 20:38:40 +0800

    This is two commit
    created by yangmingsen
    version 0.2

--- src/top/TestOne.java
+++ src/top/TestOne.java
@@ -3,6 +3,7 @@ package top;
 public class TestOne {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("this two wrtote");
 
     }
 }
---
+++ src/top/TestTwo.java
@@ -0,0 +1,7 @@
+package top;
+
+public class TestTwo {
+    public static void main(String[] args) {
+        System.out.println("Test Two");
+    }
+}
